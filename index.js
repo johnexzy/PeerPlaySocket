ba9174147e7ec1d1b0317c772c9a9dddcd7184ca
John Oba <obajohn75@gmail.com>
2023-12-08 08:02:17 +0100

    fix: double socket instance

--- index.js
+++ index.js
@@ -39,39 +39,6 @@ io.on("connect", (socket) => {
   console.log("socket connect", socket.id);
   socket.broadcast.emit("welcome", `Welcome ${socket.id}`);
 
-  socket.on("disconnect", async () => {
-    console.log("socket disconnect");
-  });
-});
-io.on("connect", (socket) => {
-  socket.on("join_room", (roomId) => {
-    socket.join(roomId);
-    console.log(`user with id-${socket.id} joined room - ${roomId}`);
-  });
-
-  socket.on("send_msg", (data) => {
-    console.log(data, "DATA");
-    // This will send a message to a specific room ID
-    console.log(data.roomId);
-    socket.to(data.roomId).emit("receive_msg", data);
-  });
-
-  socket.on("seek", (data) => {
-    console.log(data, "Seek Data");
-
-    io.to(data.roomId).emit("seek", { seekTime: data.seekTime });
-  });
-
-  socket.on("disconnect", () => {
-    console.log("A user disconnected:", socket.id);
-  });
-
-  console.log("socket connect", socket.id);
-  socket.broadcast.emit("welcome", `Welcome ${socket.id}`);
-
-  socket.on("disconnect", async () => {
-    console.log("socket disconnect");
-  });
 });
 
 const PORT = process.env.PORT || 4000;
