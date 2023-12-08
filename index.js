75a1d7fdc70b2388337cb6e13f33187e4692d966
John Oba <obajohn75@gmail.com>
2023-12-08 09:55:38 +0100

    feat: refactor on-seek emitter

--- index.js
+++ index.js
@@ -1,19 +1,45 @@
-import client from "@/utils/client";
-import { io } from "socket.io-client";
-export default function useSocket() {
-  // start the socket server
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
 
-  try {
-    const socket = io("http://localhost:4000");
+const httpServer = http.createServer();
+const PORT = process.env.PORT || 4000;
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*", // Replace with your frontend URL
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true,
+  },
+});
 
-    socket.on("connect", () => {
-      console.log("Connected");
-    });
+io.on("connect", (socket) => {
+  socket.on("join_room", (roomId) => {
+    socket.join(roomId);
+    console.log(`user with id-${socket.id} joined room - ${roomId}`);
+  });
 
-    socket.on("disconnect", () => {
-      console.log("Disconnected");
-    });
- 
-    return socket;
-  } catch (error) {}
-}
+  socket.on("send_msg", (data) => {
+    console.log(data, "DATA", socket.id);
+  
+    socket.broadcast.to(data.roomId).emit("receive_msg", data);
+  });
+
+  socket.on("seek", (data) => {
+    console.log(data, "Seek Data");
+
+    socket.broadcast.to(data.roomId).emit("seek", { seekTime: data.seekTime });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected:", socket.id);
+  });
+
+  console.log("socket connect", socket.id);
+  socket.broadcast.emit("welcome", `Welcome ${socket.id}`);
+
+});
+
+httpServer.listen(PORT, () => {
+  console.log(`Socket.io server is running on port ${PORT}`);
+});
